5ba78400353fd130205aa3dbe3322251b39f7093
Nuver <andreisidorenko28@gmail.com>
2023-11-29 19:16:12 +0200

    Create jenkinsfile.py

---
+++ jenkinsfile.py
@@ -0,0 +1,40 @@
+opipeline {
+    agent any
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                script {
+                    git 'https://github.com/Nuver2/jenkins-python-test.git'
+                }
+            }
+        }
+
+        stage('Build') {
+            steps {
+                script {
+                    sh 'python -m venv venv'
+                    sh 'source venv/bin/activate'
+                    sh 'pip install -r requirements.txt'
+                    sh 'python setup.py sdist'
+                }
+            }
+        }
+
+        stage('Test') {
+            steps {
+                script {
+                    sh 'python -m pytest'
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                script {
+                    
+                }
+            }
+        }
+    }
+}
