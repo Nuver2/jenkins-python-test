b2edb1f13cb556d78223976593c256ea92ca8cdf
Nuver2 <150943875+Nuver2@users.noreply.github.com>
2023-12-10 18:52:48 +0200

    Update jenkinsfile.py

--- jenkinsfile.py
+++ jenkinsfile.py
@@ -2,19 +2,9 @@ opipeline {
     agent any
     
     stages {
-        stage('Checkout') {
-            steps {
-                script {
-                    git clone 'git@github.com:Nuver2/jenkins-python-test.git'
-                }
-            }
-        }
-
         stage('Build') {
             steps {
                 script {
-                    sh 'python -m venv venv'
-                    sh 'source venv/bin/activate'
                     sh 'pip install -r requirements.txt'
                     sh 'python setup.py sdist'
                 }
@@ -25,14 +15,6 @@ opipeline {
             steps {
                 script {
                     sh 'python -m pytest'
-                }
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-                script {
-                    
                 }
             }
         }
